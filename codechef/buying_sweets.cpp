839ac70561f385a2d9878048d6f6d3fee1660bd3
yash3001 <yashchauhan197@gmail.com>
2021-02-17 00:58:13 +0530

    added code

---
+++ codechef/buying_sweets.cpp
@@ -0,0 +1,61 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define x first
+#define y second
+#define endl "\n"
+#define EPS 1e-9
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forf(t,i,n) for(t i=0;i<n;i++)
+#define forr(t,i,n) for(t i=n-1;i>=0;i--)
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+
+void solve(){
+    ll N, X; cin>>N>>X;
+    ll sum{};
+    vll notes(N, 0);
+    forf(ll, i, N){
+        cin>>notes.at(i);
+        sum += notes.at(i);
+    }
+    if(sum%X==0){
+        cout<<sum/X<<endl;
+        return;
+    }
+    sort(all(notes));
+    if(notes.at(0)<=sum%X){
+        cout<<-1<<endl;
+    }
+    else{
+        cout<<sum/X<<endl;
+    }
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t; cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
