25adf9d6fe113e0da647d837c58b615933a903d7
yash3001 <yashchauhan197@gmail.com>
2021-02-21 00:00:12 +0530

    added code

---
+++ codechef/mahasena.cpp
@@ -0,0 +1,60 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define x first
+#define y second
+#define endl "\n"
+#define EPS 1e-9
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forf(t,i,n) for(t i=0;i<n;i++)
+#define forr(t,i,n) for(t i=n-1;i>=0;i--)
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+
+void solve(){
+    ll n; cin>>n;
+    ll lucky{}, unlucky{};
+    forf(ll, i, n){
+        ll t; cin>>t;
+        if(t&1){
+            unlucky++;
+        }
+        else{
+            lucky++;
+        }
+    }
+    if(lucky>unlucky){
+        cout<<"READY FOR BATTLE";
+    }
+    else{
+        cout<<"NOT READY";
+    }
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t{1};
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
