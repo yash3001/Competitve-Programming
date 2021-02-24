569ea20b44299921e9b191953d1f0117e5e2acbc
yash3001 <yashchauhan197@gmail.com>
2021-02-24 14:54:48 +0530

    added code

---
+++ testing/debugging_hash.cpp
@@ -0,0 +1,13 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define deb(x) cout << #x << " " << x << endl;
+
+using namespace std;
+
+int main(){
+    int x(10), y(20);
+    deb(x);
+    deb(y);
+    return 0;
+}
