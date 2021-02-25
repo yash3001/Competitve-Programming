e661d3ce5a4a41f913f9691ea21b01dbde87dc1a
yash3001 <yashchauhan197@gmail.com>
2021-02-25 14:52:28 +0530

    added code

--- codechef/template.cpp
+++ codechef/template.cpp
@@ -3,34 +3,42 @@
 
 #define pb push_back
 #define eb emplace_back
-#define x first
-#define y second
+#define ff first
+#define ss second
 #define endl "\n"
+#define EPS 1e-9
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define forf(t,i,n) for(t i=0;i<n;i++)
 #define forr(t,i,n) for(t i=n-1;i>=0;i--)
+#define print(x) for(const auto &e: (x)) { cout<<e<<" "; } cout<<endl
 
 using namespace std;
 
 typedef long long ll;
 typedef unsigned long long ull;
+typedef long double ld;
 typedef vector<int> vi;
 typedef vector<long> vl;
-typedef vector<ll> vll;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_map<long long, long long> umll;
 
 void solve(){
-
+    
 }
 
 int main(){
     #ifndef ONLINE_JUDGE
-	    freopen("input.txt", "r", stdin);
-	    freopen("output.txt", "w", stdout);
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
     #endif
 
     ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     ll t; cin >> t;
     while(t--){
