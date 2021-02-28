2ce3f28c410fc867792f70ec099aacfc3bcd6301
yash3001 <yashchauhan197@gmail.com>
2021-02-28 21:20:14 +0530

    added code

---
+++ codechef/february_starters_2021/maximum_value.cpp
@@ -0,0 +1,63 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define EPS 1e-9
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forf(t,i,n) for(t i=0;i<n;i++)
+#define forr(t,i,n) for(t i=n-1;i>=0;i--)
+#define print(x) for(const auto &e: (x)) { cout<<e<<" "; } cout<<endl
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_map<long long, long long> umll;
+
+void solve(){
+    ll n; cin>>n;
+    vll arr(n);
+    forf(ll, i, n){
+        cin>>arr.at(i);
+    }
+    sort(all(arr));
+    ll diff{0};
+    ll a=arr.at(arr.size()-2), b=arr.at(arr.size()-1);
+    for(ll i=0; i<arr.size()-1; i++){
+        if((arr.at(i)*arr.at(i+1)+abs(arr.at(i)-arr.at(i+1)))>(a*b + abs(a-b))){
+            a=arr.at(i);
+            b=arr.at(i+1);
+        }
+    }
+    cout<<(a*b + abs(a-b))<<endl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t; cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
