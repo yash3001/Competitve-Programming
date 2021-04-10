0ca08adc3d9f851023bff4be721f4459996d461c
yash3001 <yashchauhan197@gmail.com>
2021-04-10 20:01:08 +0530

    added code

--- codechef/april_long_challenge/boolean_game_dp.cpp
+++ codechef/april_long_challenge/boolean_game_dp.cpp
@@ -39,33 +39,42 @@ T lcm(T a, T b){
 }
 
 // dp[n]
-// ll dp[61];
+// ll dp[61][61][2];
 
-// ll helper(vll &nums, vll &g, vll &d, vll &u, vll&v, ll n, ll y){
-//     if(y==1){
-//         nums.at(n)=1;
-//     }
-//     ll add = 0;
-//     for(ll i=0; i<u.size(); i++){
-//         if(n==u.at(i)){
-//             bool check=1;
-//             for(ll j=n-1; j<v.at(i); j++){
-//                 if(nums.at(j)==0){
-//                     check=0;
-//                 }
-//             }
-//             if(check){
-//                 add += d.at(i);
-//             }
-//         }
-//     }
-//     if(n==1){
-//         return(g.at(0)+add);
-//     }
-//     return(max(g.at(n-1)+add+helper(nums, g, d, u, v, n-1, 1), helper(nums, g, d, u, v, n-1, 0)));
-// }
+ll helper(vll &g, vll &u, vll &v, vll &d, ll n, ll last_pos, ll val){
+    if(n==0){
+        return 0;
+    }
+    // if(dp[n][last_pos][val]!=-1){
+    //     return dp[n][last_pos][val];
+    // }
+    ll sum = g.at(n-1);
+    if(val == 1){
+        if(last_pos==-1){
+            last_pos = n;
+        }
+    }
+    if(val == 1){
+        for(int i=0; i<u.size(); i++){
+            if(u.at(i)==n){
+                if(last_pos>=v.at(i)){
+                    sum+=d.at(i);
+                }
+            }
+        }
+    }
+    else{
+        last_pos = -1;
+    }
+    // return dp[n][last_pos][val]=(max(sum+helper(g, u, v, d, n-1, last_pos, 1), helper(g, u, v, d, n-1, -1, 0)));
+    // return (max(sum+helper(g, u, v, d, n-1, last_pos, 1), helper(g, u, v, d, n-1, -1, 0)));
+    ll max_ = (max(sum+helper(g, u, v, d, n-1, last_pos, 1), helper(g, u, v, d, n-1, -1, 0)));
+    cout<<max_<<endl;
+    return max_;
+}
 
 void solve(){
+    // memset(dp, -1, sizeof(dp));
     ll n, m, k; cin>>n>>m>>k;
     vll g;
     forf(ll, i, n){
@@ -78,50 +87,58 @@ void solve(){
         ll ui, vi, di; cin>>ui>>vi>>di;
         u.pb(ui); v.pb(vi); d.pb(di);
     }
+    cout<<helper(g, u, v, d, n, -1, 0)<<endl;
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         for(int k=0; k<2; k++){
+    //             cout<<dp[i][j][k]<<endl;
+    //         }
+    //     }
+    // }
     // cout<<helper(nums, g, d, u, v, n, 0)<<endl;
-    vll ans;
-    for(ll i=0; i<pow(2,n); i++){
-        ll count{};
-        string b;
-        ll pos{n-1};
-        // cout<<"Inside loop 1"<<endl;
-        ll curnum = i;
-        while(pos>=0){
-            if(curnum&1){
-                count+=g.at(pos);
-            }
-            b=((curnum&1)==1?'1':'0')+b;
-            curnum>>=1;
-            pos--;
-        // cout<<"Inside loop 2"<<endl;
-        }
-        // cout<<b<<" "<<count<<endl;
-        for(int j=0; j<m; j++){
-            // cout<<b.at(u.at(j)-1)<<endl;
-            if(b.at(u.at(j)-1)=='1'){
-                // cout<<"yes"<<endl;
-                bool check=1;
-                for(int k=u.at(j)-1; k<v.at(j); k++){
-                    if(b.at(k)!='1'){
-                        check=0;
-                        break;
-                    }
-                }
-                if(check){
-                    // cout<<"yes"<<endl;
-                    count+=d.at(j);
-                }
-            }
-        }
-        ans.pb(count);
-        // cout<<"Inside loop 3"<<endl;
-    }
-    // print(ans);
-    sort(all(ans));
-    for(ll i=0; i<k; i++){
-        cout<<ans.at(ans.size()-i-1)<<" ";
-    }
-    cout<<endl;
+    // vll ans;
+    // for(ll i=0; i<pow(2,n); i++){
+    //     ll count{};
+    //     string b;
+    //     ll pos{n-1};
+    //     // cout<<"Inside loop 1"<<endl;
+    //     ll curnum = i;
+    //     while(pos>=0){
+    //         if(curnum&1){
+    //             count+=g.at(pos);
+    //         }
+    //         b=((curnum&1)==1?'1':'0')+b;
+    //         curnum>>=1;
+    //         pos--;
+    //     // cout<<"Inside loop 2"<<endl;
+    //     }
+    //     // cout<<b<<" "<<count<<endl;
+    //     for(int j=0; j<m; j++){
+    //         // cout<<b.at(u.at(j)-1)<<endl;
+    //         if(b.at(u.at(j)-1)=='1'){
+    //             // cout<<"yes"<<endl;
+    //             bool check=1;
+    //             for(int k=u.at(j)-1; k<v.at(j); k++){
+    //                 if(b.at(k)!='1'){
+    //                     check=0;
+    //                     break;
+    //                 }
+    //             }
+    //             if(check){
+    //                 // cout<<"yes"<<endl;
+    //                 count+=d.at(j);
+    //             }
+    //         }
+    //     }
+    //     ans.pb(count);
+    //     // cout<<"Inside loop 3"<<endl;
+    // }
+    // // print(ans);
+    // sort(all(ans));
+    // for(ll i=0; i<k; i++){
+    //     cout<<ans.at(ans.size()-i-1)<<" ";
+    // }
+    // cout<<endl;
 }
 
 int main(){
