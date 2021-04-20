11f98b401c891ffa719128bef0c7e84b469492d3
yash3001 <yashchauhan197@gmail.com>
2021-04-20 23:42:37 +0530

    added code

--- algorithms/Dynamic_Programming/2. Unbounded Knapsack/4. Coin Change Problem: Minimum Coins/coin_change_minimum_coins.cpp
+++ algorithms/Dynamic_Programming/2. Unbounded Knapsack/4. Coin Change Problem: Minimum Coins/coin_change_minimum_coins.cpp
@@ -1,41 +1,49 @@
 /* @author -> gamma30 */
 #include <bits/stdc++.h>
+#include <climits>
+#include <cstdint>
 
 using namespace std;
 
-int coin_change(vector<int> value, int sum, int n);
+int coin_change(vector<int> coins, int sum, int n);
 
 int dp[1001][1001];
 
 int main(){
-    memset(dp, -1, sizeof(dp));
     cout<<"Enter the number of coins: ";
     int n; cin>>n;
 
-    vector<int> value(n);
-    cout<<"Enter the value of the elements:"<<endl;
+    vector<int> coins(n);
+    cout<<"Enter the value of the elements: ";
     for(int i=0; i<n; i++){
-        cin>>value[i];
+        cin>>coins[i];
     }
 
     int sum{};
     cout<<"Enter the sum: ";
     cin>>sum;
-    cout<<"The number of ways are: "<<coin_change(value, sum, n)<<endl;
+    cout<<"The minimum number of coins are: "<<coin_change(coins, sum, n)<<endl;
     return 0;
 }
 
-int coin_change(vector<int> value, int sum, int n){
-    for(int i=1; i<sum+1; i++){
-        dp[0][i] = 0;
-    }
-    for(int i=0; i<n+1; i++){
-        dp[i][0] = 1;
+int coin_change(vector<int> coins, int sum, int n){
+    for(int i=0; i<sum+1; i++){
+        dp[0][i] = INT_MAX-1;
     }
     for(int i=1; i<n+1; i++){
+        dp[i][0] = 0;
+    }
+    for(int i=1; i<sum+1; i++){
+        if(i%coins[0]==0)
+            dp[1][i] = i/coins[0];
+        else{
+            dp[1][i] = INT_MAX-1;
+        }
+    }
+    for(int i=2; i<n+1; i++){
         for(int j=1; j<sum+1; j++){
-            if(value[i-1]<=j){
-                dp[i][j] = dp[i][j-value[i-1]]+dp[i-1][j];
+            if(coins[i-1]<=sum){
+                dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
             }
             else{
                 dp[i][j] = dp[i-1][j];
