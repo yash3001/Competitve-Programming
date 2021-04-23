aca23507a90cd978bd0a6a45320a57b5757f047b
yash3001 <yashchauhan197@gmail.com>
2021-04-23 23:38:22 +0530

    added code

---
+++ algorithms/Dynamic_Programming/3. Longest Common Subsequence (LCS)/5. Minimum Number of Insertions And Deletions To Convert One String To Another/minimum_number_of_insertions_and_deletions_top_down.cpp
@@ -0,0 +1,41 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int longest_common_subsequence_length(string &s1, string &s2, int m, int n);
+
+int dp[1001][1001];
+
+int main(){
+    cout<<"Enter the first string: ";
+    string s1; cin>>s1;
+    cout<<"Enter the second string: ";
+    string s2; cin>>s2;
+    int l = longest_common_subsequence_length(s1, s2, s1.length(), s2.length());
+    cout<<"Minimum deletions: "<<s1.length()-l<<" \nMinimum insertions: "<<s2.length()-l<<endl;
+    return 0;
+}
+
+int longest_common_subsequence_length(string &s1, string &s2, int m, int n){
+
+    for(int i=0; i<m+1; i++){
+        dp[i][0] = 0;
+    }
+    for(int i=1; i<n+1; i++){
+        dp[0][i] = 0;
+    }
+
+    for(int i=1; i<m+1; i++){
+        for(int j=1; j<n+1; j++){
+            if(s1[i-1] == s2[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
