54e24f62bb93a936d0e34ef4ccfcc359da73fe15
yash3001 <yashchauhan197@gmail.com>
2021-04-24 18:07:46 +0530

    added code

---
+++ algorithms/Dynamic_Programming/3. Longest Common Subsequence (LCS)/11. Length Of Longest Repeating Subsequence/longest_repeating_subsequence_length_top_down.cpp
@@ -0,0 +1,39 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int longest_repeating_subsequence_length(string &s1, string &s2, int m, int n);
+
+int dp[1001][1001];
+
+int main(){
+    cout<<"Enter the string: ";
+    string s1; cin>>s1;
+    string s2 = s1;
+    cout<<"The length of the longest repeating subsequence is "<<longest_repeating_subsequence_length(s1, s2, s1.length(), s2.length())<<endl;
+    return 0;
+}
+
+int longest_repeating_subsequence_length(string &s1, string &s2, int m, int n){
+
+    for(int i=0; i<m+1; i++){
+        dp[i][0] = 0;
+    }
+    for(int i=1; i<n+1; i++){
+        dp[0][i] = 0;
+    }
+
+    for(int i=1; i<m+1; i++){
+        for(int j=1; j<n+1; j++){
+            if(s1[i-1] == s2[j-1] && i!=j){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
---
+++ algorithms/Dynamic_Programming/3. Longest Common Subsequence (LCS)/12. Print Longest Repeating Subsequence/print_longest_repeating_subsequence.cpp
@@ -0,0 +1,53 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string longest_repeating_subsequence(string &s1, string &s2, int m, int n);
+
+int dp[1001][1001];
+
+int main(){
+    cout<<"Enter the string: ";
+    string s1; cin>>s1;
+    string s2=s1;
+    cout<<"The longest repeating subsequence is "<<longest_repeating_subsequence(s1, s2, s1.length(), s2.length())<<endl;
+    return 0;
+}
+
+string longest_repeating_subsequence(string &s1, string &s2, int m, int n){
+    for(int i=0; i<m+1; i++){
+        dp[i][0] = 0;
+    }
+    for(int i=0; i<n+1; i++){
+        dp[0][i] = 0;
+    }
+    for(int i=1; i<m+1; i++){
+        for(int j=1; j<n+1; j++){
+            if(s1[i-1]==s2[j-1] && i!=j){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    int i=m, j=n;
+    string ans;
+    while(i != 0 && j != 0){
+        if(s1[i-1]==s2[j-1] && i!=j){
+            ans = s1[i-1]+ans;
+            i--;
+            j--;
+        }
+        else{
+            if(dp[i-1][j]>dp[i][j-1]){
+                i--;
+            }
+            else{
+                j--;
+            }
+        }
+    }
+    return ans;
+}
