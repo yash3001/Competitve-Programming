cb4a95203a8ce351336873106a0524ebdeae84de
yash3001 <yashchauhan197@gmail.com>
2021-04-26 00:38:01 +0530

    added code

---
+++ algorithms/Dynamic_Programming/4. Matrix Chain Multiplication (MCM)/2. Palindrome Partitioning/palindrome_partitioning_memoization.cpp
@@ -0,0 +1,41 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int palindrome_partitioning(string &s, int i, int j);
+
+int dp[1001][1001];
+
+int main(){
+    memset(dp, -1, sizeof(dp));
+    cout<<"Enter the string: ";
+    string s; cin>>s;
+    cout<<"The minimum number of partitioning can needs to be done is "<<palindrome_partitioning(s, 0, s.size()-1)<<endl;
+    return 0;
+}
+
+int palindrome_partitioning(string &s, int i, int j){
+    if(i>=j){
+        return dp[i][j]=0;
+    }
+    
+    if(dp[i][j] != -1){
+        return dp[i][j];
+    }
+
+    string t=s.substr(i, j-i+1);
+    reverse(t.begin(), t.end());
+    if(t==s.substr(i, j-i+1)){
+        return dp[i][j]=0;
+    }
+
+    int min=INT_MAX;
+    for(int k=i; k<j; k++){
+        int temp = palindrome_partitioning(s, i, k) + palindrome_partitioning(s, k+1, j) + 1;
+        if(temp < min){
+            min = temp;
+        }
+    }
+    return dp[i][j]=min;
+}
---
+++ algorithms/Dynamic_Programming/4. Matrix Chain Multiplication (MCM)/2. Palindrome Partitioning/palindrome_partitioning_recursion.cpp
@@ -0,0 +1,34 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int palindrome_partitioning(string &s, int i, int j);
+
+int main(){
+    cout<<"Enter the string: ";
+    string s; cin>>s;
+    cout<<"The minimum number of partitioning can needs to be done is "<<palindrome_partitioning(s, 0, s.size()-1)<<endl;
+    return 0;
+}
+
+int palindrome_partitioning(string &s, int i, int j){
+    if(i>=j){
+        return 0;
+    }
+
+    string t=s.substr(i, j-i+1);
+    reverse(t.begin(), t.end());
+    if(t==s.substr(i, j-i+1)){
+        return 0;
+    }
+
+    int min=INT_MAX;
+    for(int k=i; k<j; k++){
+        int temp = palindrome_partitioning(s, i, k) + palindrome_partitioning(s, k+1, j) + 1;
+        if(temp < min){
+            min = temp;
+        }
+    }
+    return min;
+}
