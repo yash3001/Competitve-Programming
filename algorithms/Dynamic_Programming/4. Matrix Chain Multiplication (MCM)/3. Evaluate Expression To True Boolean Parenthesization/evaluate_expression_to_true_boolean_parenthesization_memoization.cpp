431eb0bde949c83fe0ab69d5c35ca7648c6c07b2
yash3001 <yashchauhan197@gmail.com>
2021-04-26 15:09:51 +0530

    added code

--- algorithms/Dynamic_Programming/4. Matrix Chain Multiplication (MCM)/3. Evaluate Expression To True Boolean Parenthesization/evaluate_expression_to_true_boolean_parenthesization_memoization.cpp
+++ algorithms/Dynamic_Programming/4. Matrix Chain Multiplication (MCM)/3. Evaluate Expression To True Boolean Parenthesization/evaluate_expression_to_true_boolean_parenthesization_memoization.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 int boolean_parenthesization(string s, int i, int j, int isTrue);
 
+int dp[1001][1001][2];
+
 int main(){
+    memset(dp, -1, sizeof(dp));
     cout<<"Enter the string: ";
     string s; cin>>s;
     cout<<"The number of ways of parenthesization such that the expression evaluates to true are "<<boolean_parenthesization(s, 0, s.size()-1, 1)<<endl;
@@ -14,22 +17,53 @@ int main(){
 
 int boolean_parenthesization(string s, int i, int j, int isTrue){
     if(i>j){
-        return false;
+        return dp[i][j][isTrue]=false;
     }
     if(i==j){
         if(isTrue){
-            return s[i]=='T' || s[i]=='t';
+            return dp[i][j][isTrue]=s[i]=='T' || s[i]=='t';
         }
         else{
-            return s[i]=='F' || s[i]=='f';
+            return dp[i][j][isTrue]=s[i]=='F' || s[i]=='f';
         }
     }
+
+    if(dp[i][j][isTrue] != -1){
+        return dp[i][j][isTrue];
+    }
+
     int count{};
     for(int k=i+1; k<j; k=k+2){
-        int left_t = boolean_parenthesization(s, i, k-1, 1);
-        int left_f = boolean_parenthesization(s, i, k-1, 0);
-        int right_t = boolean_parenthesization(s, k+1, j, 1);
-        int right_f = boolean_parenthesization(s, k+1, j, 0);
+
+        int left_t;
+        if(dp[i][k-1][1] != -1){
+            left_t = dp[i][k-1][1];
+        }
+        else{
+            left_t = boolean_parenthesization(s, i, k-1, 1);
+        }
+        int left_f;
+        if(dp[i][k-1][0] != -1){
+            left_f = dp[i][k-1][0];
+        }
+        else{
+            left_f = boolean_parenthesization(s, i, k-1, 0);
+        }
+        int right_t;
+        if(dp[k+1][j][1] != -1){
+            right_t = dp[k+1][j][1];
+        }
+        else{
+            right_t = boolean_parenthesization(s, k+1, j, 1);
+        }
+        int right_f;
+        if(dp[k+1][j][0] != -1){
+            right_f = dp[k+1][j][0];
+        }
+        else{
+            right_f = boolean_parenthesization(s, k+1, j, 0);
+        }
+
         if(s[k]=='|'){
             if(isTrue)
                 count += left_t*right_t + left_t*right_f + left_f*right_t;
@@ -49,5 +83,5 @@ int boolean_parenthesization(string s, int i, int j, int isTrue){
                 count += left_t*right_t + left_f*right_f;
         }
     }
-    return count;
+    return dp[i][j][isTrue]=count;
 }
