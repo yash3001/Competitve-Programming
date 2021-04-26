fd74983002529f22821cd010a46fd1ae51614988
yash3001 <yashchauhan197@gmail.com>
2021-04-26 22:35:59 +0530

    added code

---
+++ algorithms/Dynamic_Programming/4. Matrix Chain Multiplication (MCM)/4. Scrambled String/scrambled_string_recursion.cpp
@@ -0,0 +1,41 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool scrambled_string(string s1, string s2);
+
+int main(){
+    cout<<"Enter the first string: ";
+    string s1; cin>>s1;
+    cout<<"Enter the second string: ";
+    string s2; cin>>s2;
+    if(scrambled_string(s1, s2)){
+        cout<<"Second string is scrambled"<<endl;
+    }
+    else{
+        cout<<"Second string is not scrambled"<<endl;
+    }
+    return 0;
+}
+
+bool scrambled_string(string s1, string s2){
+    if(s1==s2){
+        return true;
+    }
+    if(s1.size() != s2.size()){
+        return false;
+    }
+    if(s1.length() <= 1){
+        return false;
+    }
+    int n = s1.length();
+    bool check = false;
+    for(int k=1; k<n; k++){
+        check = ( scrambled_string(s1.substr(0, k), s2.substr(0, k)) && scrambled_string(s1.substr(k, n-k), s2.substr(k, n-k)) ) || ( scrambled_string(s1.substr(0, k), s2.substr(n-k, k)) && scrambled_string(s1.substr(k, n-k), s2.substr(0, n-k)) );
+        if(check == true){
+            break;
+        }
+    }
+    return check;
+}
