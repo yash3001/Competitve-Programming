aad66056915bfec4b1ad89291301f5cd1344f5bd
yash3001 <yashchauhan197@gmail.com>
2021-05-01 00:07:54 +0530

    added code

---
+++ Love Babbar 450/Bit Manipulation/2_count_bits_to_be_flipped.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        
+        // Your logic here
+        int n = a^b;
+        int ans{};
+        while(n>0){
+            if(n&1){
+                ans++;
+            }
+            n>>=1;
+        }
+        return ans;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;// input the testcases
+	while(t--) //while testcases exist
+	{
+		int a,b;
+		cin>>a>>b; //input a and b
+
+        Solution ob;
+		cout<<ob.countBitsFlip(a, b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
