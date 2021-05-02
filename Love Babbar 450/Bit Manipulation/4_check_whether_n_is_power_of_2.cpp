13751571ddd5501c5ff272bc69964fcfd7e5e25b
yash3001 <yashchauhan197@gmail.com>
2021-05-02 16:37:04 +0530

    added code

---
+++ Love Babbar 450/Bit Manipulation/4_check_whether_n_is_power_of_2.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here    
+        long long ans{};
+        if(n==0){
+            return false;
+        }
+        while(n>0){
+            if(n&1){
+                ans++;
+            }
+            if(ans>1){
+                return false;
+            }
+            n>>=1;
+        }
+        return true;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+
+        Solution ob;
+         if(ob.isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
