687362e996d5cf092bcc1b67c11db26e38a5fac3
yash3001 <yashchauhan197@gmail.com>
2021-05-02 16:42:01 +0530

    added code

---
+++ Love Babbar 450/Bit Manipulation/5_find_pos_of_only_set_bit.cpp
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int findPosition(int n) {
+        // code here
+        int ans{}, count{};
+        while(n>0){
+            if(n&1){
+                count++;
+            }
+            ans++;
+            n>>=1;
+        }
+        if(count!=1){
+            return -1;
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.findPosition(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
