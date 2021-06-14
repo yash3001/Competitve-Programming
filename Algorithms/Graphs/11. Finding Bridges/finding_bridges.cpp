5452bda3c896855efc0053980583f444a6ece5b8
Yash Chauhan <yashchauhan197@gmail.com>
2021-06-14 17:27:26 +0530

    added code

---
+++ Algorithms/Graphs/11. Finding Bridges/finding_bridges.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> in(1001, 0), low(1001, 0), visited(1001, 0);
+vector<vector<int>> adjacency_list(1001);
+int timer=0;
+
+void dfs(int v, int p){
+    visited.at(v) = 1;
+    low.at(v) = in.at(v) = timer++;
+    for(int n: adjacency_list.at(v)){
+        if(n == p){
+            continue;
+        }
+        else if(visited.at(n)){
+            low.at(v) = min(low.at(v), in.at(n));
+        }
+        else{
+            dfs(n, v);
+            low.at(v) = min(low.at(v), low.at(n));
+            if(low.at(n)>in.at(v)){
+                cout<<v<<"-"<<n<<" is a bridge"<<endl;
+            }
+        }
+    }
+}
+
+int main(){
+    cout<<"Enter the number of nodes: ";
+    int n; cin>>n;
+    cout<<"Enter the number of edges: ";
+    int m; cin>>m;
+    cout<<"Enter the graph\n";
+    while(m--){
+        int a, b; cin>>a>>b;
+        adjacency_list.at(a).push_back(b);
+        adjacency_list.at(b).push_back(a);
+    }
+    vector<int> visited(n+1, 0), in(n+1, 0), low(n+1, 0);
+    dfs(1, -1);
+    return 0;
+}
