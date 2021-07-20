a4d2a9771bfbb90f0ac1dcf6a4dc8a21e604ed74
Yash Chauhan <yashchauhan197@gmail.com>
2021-07-20 19:24:55 +0530

    added code

---
+++ Codeforces/Practice/800/watermelon.cpp
@@ -0,0 +1,74 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define EPS 1e-9
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forf(t,i,n) for(t i=0;i<n;i++)
+#define forr(t,i,n) for(t i=n-1;i>=0;i--)
+#define print(x) for(const auto &e: (x)) { cout<<e<<" "; } cout<<endl
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_map<long long, long long> umll;
+
+template<typename T>
+T gcd(T a, T b){
+    if(b == 0)
+        return a;
+    return(gcd<T>(b, a%b));
+}
+
+template<typename T>
+T lcm(T a, T b){
+    return (a / gcd<T>(a, b)) * b;
+}
+
+template<typename T>
+void swap_(T &a, T &b){
+    a = a^b;
+    b = b^a;
+    a = a^b;
+}
+
+void solve(){
+    ll w; cin>>w;
+    if(w&1 || w==2){
+        cout<<"NO"<<endl;
+        return;
+    }
+    cout<<"YES"<<endl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // ll t; cin >> t;
+    ll t = 1;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
