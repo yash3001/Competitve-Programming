959ef579426e1c176b7e19565d8d6f94a5848aa9
Yash Chauhan <yashchauhan197@gmail.com>
2021-08-10 02:40:56 +0530

    added code

---
+++ Codeforces/2) Round 737 (div 2)/moamen_and_k-subarrays.cpp
@@ -0,0 +1,106 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define EPS 1e-9
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forf(t,i,n) for(t i=0;i<n;i++)
+#define forr(t,i,n) for(t i=n-1;i>=0;i--)
+#define print(x) for(const auto &e: (x)) { cout<<e<<" "; } cout<<endl
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_map<long long, long long> umll;
+
+template<typename T>
+T gcd(T a, T b){
+    if(b == 0)
+        return a;
+    return(gcd<T>(b, a%b));
+}
+
+template<typename T>
+T lcm(T a, T b){
+    return (a / gcd<T>(a, b)) * b;
+}
+
+template<typename T>
+void swap_(T &a, T &b){
+    a = a^b;
+    b = b^a;
+    a = a^b;
+}
+
+void solve(){
+    ll n, k; cin>>n>>k;
+    vll nums, v;
+    forf(ll, i, n){
+        ll t; cin>>t;
+        v.pb(t);
+    }
+    nums = v;
+
+    sort(v.begin(), v.end());
+    umll mp;
+    for(int i=0; i<v.size(); i++){
+        mp[v[i]] = i;
+    }
+
+    ll count=0;
+    bool sort_start = false;
+    for(int i=1; i<n; i++){
+        if(nums[i-1] > nums[i]){
+            count++;
+        }
+        else{
+            if(!sort_start){
+                count++;
+                sort_start = true;
+            }
+            if(mp[nums[i]] - mp[nums[i-1]] != 1){
+                sort_start = false;
+            }
+        }
+    }
+    if(!sort_start){
+        count++;
+    }
+    if(k >= count){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t; cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
