ac92e48f91da24d98dab778c6a039ebc1737110c
Yash Chauhan <yashchauhan197@gmail.com>
2021-08-14 02:37:35 +0530

    added code

---
+++ Codeforces/Practice/1300/sort_the_array.cpp
@@ -0,0 +1,112 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define EPS 1e-9
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forf(t,i,n) for(t i=0;i<n;i++)
+#define forr(t,i,n) for(t i=n-1;i>=0;i--)
+#define print(x) for(const auto &e: (x)) { cout<<e<<" "; } cout<<endl
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_map<long long, long long> umll;
+
+template<typename T>
+T gcd(T a, T b){
+    if(b == 0)
+        return a;
+    return(gcd<T>(b, a%b));
+}
+
+template<typename T>
+T lcm(T a, T b){
+    return (a / gcd<T>(a, b)) * b;
+}
+
+template<typename T>
+void swap_(T &a, T &b){
+    a = a^b;
+    b = b^a;
+    a = a^b;
+}
+
+void solve(){
+    ll n; cin>>n;
+    vll nums;
+    while(n--){
+        ll t; cin>>t;
+        nums.pb(t);
+    }
+    vll s_nums = nums;
+    sort(all(s_nums));
+
+    int count{};
+    vll segment, s_segment;
+    n = nums.size();
+    ll i{};
+    ll l, r;
+    while(i < n){
+        cout<<i<<endl;
+        if(nums[i] != s_nums[i]){
+            l = i+1;
+            count++;
+            if(count == 2){
+                cout<<"no_1";
+                return;
+            }
+            while(i < n && nums[i] != s_nums[i]){
+                segment.pb(nums[i]);
+                s_segment.pb(s_nums[i]);
+                i++;
+            }
+            r = i;
+        }
+        i++;
+    }
+    if(count == 0){
+        cout<<"yes"<<endl;
+        cout<<"1 1";
+        return;
+    }
+    sort(all(segment));
+    forf(ll, i, segment.size()){
+        if(segment.at(i) != s_segment.at(i)){
+            cout<<"no_2";
+            return;
+        }
+    }
+    cout<<"yes"<<endl<<l<<" "<<r;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t{1};
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
