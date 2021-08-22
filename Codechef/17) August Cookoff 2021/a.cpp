b8a81db4619550b65136a5a74c1d350f618cea6e
Yash Chauhan <yashchauhan197@gmail.com>
2021-08-23 01:16:27 +0530

    added code

---
+++ Codechef/17) August Cookoff 2021/a.cpp
@@ -0,0 +1,105 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define EPS 1e-9
+#define MOD 1e9+7
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forf(t,i,n) for(t i=0;i<n;i++)
+#define forr(t,i,n) for(t i=n-1;i>=0;i--)
+#define print(x) for(const auto &e: (x)) { cout<<e<<" "; } cout<<endl
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_map<long long, long long> umll;
+
+template<typename T>
+T gcd(T a, T b){
+    if(b == 0)
+        return a;
+    return(gcd<T>(b, a%b));
+}
+
+template<typename T>
+T lcm(T a, T b){
+    return (a / gcd<T>(a, b)) * b;
+}
+
+template<typename T>
+void swap_(T &a, T &b){
+    a = a^b;
+    b = b^a;
+    a = a^b;
+}
+
+template<typename T>
+T modpow(T a, T b, T m){
+    if(b == 0){
+        return 1;
+    }
+    T c = modpow(a, b/2, m);
+    c = (c * c)%m;
+    if(b%2 == 1){
+        c = (c * a)%m;
+    }
+    return c;
+}
+
+void solve(){
+    ll n, max_v=0, v; cin>>n;
+    vll uniq, nums;
+    unordered_map<ll, ll> mp;
+    for(ll i=0; i<n; i++){
+        ll t; cin>>t;
+        if(!mp[t]){
+            uniq.push_back(t);
+        }
+        mp[t]++;
+        if(mp[t] > max_v){
+            max_v = mp[t];
+            v = t;
+        }
+        nums.pb(t);
+    }
+    if(nums.size() <= 2){
+        cout<<0<<endl;
+        return;
+    }
+    if(uniq.size() == nums.size()){
+        cout<<nums.size()-2<<endl;
+        return;
+    }
+    cout<<nums.size()-max_v<<endl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t; cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
---
+++ Codechef/17) August Cookoff 2021/c.cpp
@@ -0,0 +1,141 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define EPS 1e-9
+#define MOD 1e9+7
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forf(t,i,n) for(t i=0;i<n;i++)
+#define forr(t,i,n) for(t i=n-1;i>=0;i--)
+#define print(x) for(const auto &e: (x)) { cout<<e<<" "; } cout<<endl
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_map<long long, long long> umll;
+
+template<typename T>
+T gcd(T a, T b){
+    if(b == 0)
+        return a;
+    return(gcd<T>(b, a%b));
+}
+
+template<typename T>
+T lcm(T a, T b){
+    return (a / gcd<T>(a, b)) * b;
+}
+
+template<typename T>
+void swap_(T &a, T &b){
+    a = a^b;
+    b = b^a;
+    a = a^b;
+}
+
+template<typename T>
+T modpow(T a, T b, T m){
+    if(b == 0){
+        return 1;
+    }
+    T c = modpow(a, b/2, m);
+    c = (c * c)%m;
+    if(b%2 == 1){
+        c = (c * a)%m;
+    }
+    return c;
+}
+
+void solve(){
+    ll n; cin>>n;
+    if(n == 1){
+        ll t; cin>>t;
+        cout<<1<<endl;
+        return;
+    }
+    ll neg=0, pos=0, mone=0, one = 0, zero=0;
+    forf(ll, i, n){
+        ll t; cin>>t;
+        if(t < -1){
+            neg++;
+        }
+        if(t == -1){
+            mone++;
+        }
+        if(t == 0){
+            zero++;
+        }
+        if(t == 1 ){
+            one++;
+        }
+        if( t > 1){
+            pos++;
+        }
+    }
+
+    if(mone>0){
+        if(mone+zero == n){
+            if(mone == 1){
+                cout<<1<<endl;
+                return;
+            }
+            cout<<0<<endl;
+            return;
+        }
+        if(pos == 0 && neg == 0){
+            if(one > 0){
+                cout<<1<<endl;
+                return;
+            }
+            else{
+                cout<<0<<endl;
+                return;
+            }
+        }
+        else{
+            cout<<0<<endl;
+            return;
+        }
+    }
+
+    if(pos > 1 || neg > 1){
+        cout<<0<<endl;
+        return;
+    }
+    if(pos+neg > 1){
+        cout<<0<<endl;
+        return;
+    }
+    cout<<1<<endl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t; cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
