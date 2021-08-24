e02f07ad13a7cf30aa94b9f24f9e1c05e5c89c9c
Yash Chauhan <yashchauhan197@gmail.com>
2021-08-25 00:19:34 +0530

    added code

---
+++ Codeforces/5) Round 740 (div 2)/simply_strange_sort.cpp
@@ -0,0 +1,137 @@
+/* @author -> gamma30 */
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define EPS 1e-9
+#define MOD 1e9+7
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forf(t,i,n) for(t i=0;i<n;i++)
+#define forr(t,i,n) for(t i=n-1;i>=0;i--)
+#define print(x) for(const auto &e: (x)) { cout<<e<<" "; } cout<<endl
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_map<long long, long long> umll;
+
+template<typename T>
+T gcd(T a, T b){
+    if(b == 0)
+        return a;
+    return(gcd<T>(b, a%b));
+}
+
+template<typename T>
+T lcm(T a, T b){
+    return (a / gcd<T>(a, b)) * b;
+}
+
+template<typename T>
+void swap_(T &a, T &b){
+    a = a^b;
+    b = b^a;
+    a = a^b;
+}
+
+template<typename T>
+T modpow(T a, T b, T m){
+    if(b == 0){
+        return 1;
+    }
+    T c = modpow(a, b/2, m);
+    c = (c * c)%m;
+    if(b%2 == 1){
+        c = (c * a)%m;
+    }
+    return c;
+}
+
+void solve(){
+    ll n; cin>>n;
+    vll nums, temp;
+    forf(ll, i, n){
+        ll t; cin>>t;
+        nums.pb(t);
+        temp.pb(t);
+    }
+    sort(all(temp));
+    bool sorted = true;
+    forf(ll, i, n){
+        if(nums[i] != temp[i]){
+            sorted = false;
+            break;
+        }
+    }
+    if(sorted){
+        cout<<0<<endl;
+        return;
+    }
+
+    ll ans = 0;
+
+    ll k = 1;
+    while(true){
+        if(k&1){
+            for(ll i = 0; i<=n-2; i+=2){
+                if(nums[i] > nums[i+1]){
+                    swap(nums[i], nums[i+1]);
+                }
+            }
+        }
+        else{
+            for(ll i = 1; i<=n-1; i+=2){
+                if(nums[i] > nums[i+1]){
+                    swap(nums[i], nums[i+1]);
+                }
+            }
+        }
+
+        ans++;
+        k++;
+
+        sorted = true;
+        forf(ll, j, n){
+            if(nums[j] != temp[j]){
+                sorted = false;
+                break;
+            }
+        }
+        if(sorted){
+            cout<<ans<<endl;
+            return;
+        }
+    }
+}
+
+int main(){
+
+    // cfh - ctrl+alt+b
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t; cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
