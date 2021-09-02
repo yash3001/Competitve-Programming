1a2f1b6adde7b3a7722a30b9e1e21ec0af15836f
Yash Chauhan <yashchauhan197@gmail.com>
2021-09-02 19:55:41 +0530

    added code

--- CSES/Problems/03. Dynamic Programming/04_coin_combinations_II.cpp
+++ CSES/Problems/03. Dynamic Programming/04_coin_combinations_II.cpp
@@ -1,3 +1,6 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
 /* @author -> gamma30 */
 #include <bits/stdc++.h>
 
@@ -66,25 +69,31 @@ void solve(){
         cin>>coins[i];
     }
 
-    int dp[x+1][n+1];
-    for(int i=1; i<x+1; i++){
-        dp[i][0] = 0;
-    }
+    // print(coins);
+
+    vvi dp(n+1, vi(x+1, 0));
+
+    // dp[0][0] = 1;
+
     for(int i=0; i<n+1; i++){
-        dp[0][i] = 1;
+        dp[i][0] = 1;
     }
     for(int i=1; i<x+1; i++){
-        for(int j=1; j<n+1; j++){
-            if(coins[j-1] <= i){
-                (dp[i][j] = dp[i-coins[j-1]][j] + dp[i][j-1]) %= 1'000'000'007;
+        dp[0][i] = 0;
+    }
+
+    for(int i=1; i<n+1; i++){
+        for(int j=1; j<x+1; j++){
+            if(coins[i-1] <= j){
+                dp[i][j] = (dp[i][j-coins[i-1]] + dp[i-1][j])%1000000007;
             }
             else{
-                dp[i][j] = dp[i][j-1];
+                dp[i][j] = dp[i-1][j];
             }
         }
     }
 
-    cout<<dp[x][n];
+    cout<<dp[n][x];
 }
 
 int main(){
