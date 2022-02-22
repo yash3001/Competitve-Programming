fd902ae453e00d4d69abdfd85771c4489af08913
Yash Chauhan <yashchauhan197@gmail.com>
2022-02-23 01:22:48 +0530

    added code

--- template.cpp
+++ template.cpp
@@ -1,12 +1,13 @@
 // @author ↓
-//
-//  ▄████  ▄▄▄       ███▄ ▄███▓ ███▄ ▄███▓ ▄▄▄
-// ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓██▒▀█▀ ██▒▒████▄
-//▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▓██    ▓██░▒██  ▀█▄
-//░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒██    ▒██ ░██▄▄▄▄██
-//░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒▒██▒   ░██▒ ▓█   ▓██▒
-// ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░ ▒░   ░  ░ ▒▒   ▓▒█░
-//  ░   ░   ▒   ▒▒ ░░  ░      ░░  ░      ░  ▒   ▒▒ ░
+
+//  ██ ██     ██                     ██                    ██      ██████   ██████████ ██       ██
+// ░██░██    ░██                    ████                  ░██     ░█░░░░██ ░░░░░██░░░ ░██      ░██
+// ░██░██    ░██  ██████  █████    ██░░██   ██████  █████ ░██     ░█   ░██     ░██    ░██   █  ░██
+// ░██░██    ░██ ██░░░░  ██░░░██  ██  ░░██ ░░██░░█ ██░░░██░██████ ░██████      ░██    ░██  ███ ░██
+// ░██░██    ░██░░█████ ░███████ ██████████ ░██ ░ ░██  ░░ ░██░░░██░█░░░░ ██    ░██    ░██ ██░██░██
+// ░██░██    ░██ ░░░░░██░██░░░░ ░██░░░░░░██ ░██   ░██   ██░██  ░██░█    ░██    ░██    ░████ ░░████
+// ░██░░███████  ██████ ░░██████░██     ░██░███   ░░█████ ░██  ░██░███████     ░██    ░██░   ░░░██
+// ░░  ░░░░░░░  ░░░░░░   ░░░░░░ ░░      ░░ ░░░     ░░░░░  ░░   ░░ ░░░░░░░      ░░     ░░       ░░ 
 
 // #pragma GCC optimize("Ofast")
 // #pragma GCC target("avx,avx2,fma")
@@ -36,6 +37,7 @@ using namespace __gnu_pbds;
 #define ceach(a,x) for(const auto &a: x)
 #define each(a,x) for(auto &a: x)
 #define print(x) for(const auto &e: (x)) { cout<<e<<" "; } cout<<endl
+#define daalo(a) each(x, a) { cin>>x; }
 
 using namespace std;
 
@@ -117,6 +119,22 @@ template<typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, 
 template<typename T> using ordered_set_dec = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<typename T> using ordered_multiset_dec = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = mod_mul(res , a, mod); a = mod_mul(a , a ,mod); b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+
 template<typename T>
 T gcd(T a, T b){
     if(b == 0)
@@ -149,6 +167,54 @@ T modpow(T a, T b, T m){
     return c;
 }
 
+vector<long long> primeFactorization(long long n) {
+    vector<long long> factorization;
+    for (int d : {2, 3, 5}) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+    }
+    static array<int, 8> increments = {4, 2, 4, 2, 4, 6, 2, 6};
+    int i = 0;
+    for (long long d = 7; d * d <= n; d += increments[i++]) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+        if (i == 8)
+            i = 0;
+    }
+    if (n > 1)
+        factorization.push_back(n);
+    return factorization;
+}
+
+vector<long long> divisors(long long n) {
+    vector<long long> divisors;
+    for(long long i = 1; i * i <= n; i++){
+        if(n % i == 0){
+            divisors.push_back(i);
+            if(n/i != i){
+                divisors.push_back(n/i);
+            }
+        }
+    }
+    return divisors; 
+}
+
+vector<bool> seive(long long n){
+    vector<bool> is_prime(n+1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (long long i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (long long j = i * i; j <= n; j += i)
+                is_prime[j] = false;
+        }
+    }
+    return is_prime;
+}
+
 /* ----------STRING AND INTEGER CONVERSIONS---------- */
 // 1) number to string -> to_string(num)
 // 2) string to number -> stoi(str)
@@ -161,6 +227,11 @@ T modpow(T a, T b, T m){
 // pbds<ll> s; (almost same as set)
 // s.find_by_order(i)    0<=i<n     returns iterator to ith element (0 if i>=n)
 // s.order_of_key(e)     returns elements strictly less than the given element e (need not be present)
+
+/* ------------------Binary Search------------------ */
+// 1) Lower Bound -> returns iterator to the first element greater than or equal to the given element or returns end() if no such element exists
+// 2) Upper Bound -> returns iterator to the first element greater than the given element or returns end() if no such element exists
+
 
 void solve(){
     
